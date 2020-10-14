c189eba10972204e67118e40df51bab090b0adf6
LukacTomas <lukactv@gmail.com>
2020-10-14 08:35:09 +0200

    added scanf3

---
+++ uvod_do_C/Scanf3.c
@@ -0,0 +1,49 @@
+/**
+ * Subor:   Cvicenie.c
+ *
+ * Autor1:  Tomas Lukac (lukac@fajnorka.sk)
+ * Datum:   04. oktobra 2020
+ * Partner: Pracoval som sám
+ * Predmet: PRJ 2020
+ * Trieda:  Učiteľ
+ *
+ * Opis suboru:
+ *
+ *  Napisme program, ktory spocita obsah a obvod kruhu.
+ *
+ */
+#include <stdio.h>
+
+int main(void)
+{
+
+    char znak;
+    printf("Zadaj char: ");
+    scanf("%c", &znak);
+    printf("Tvoj znak je %c\n", znak);
+
+    int cislo;
+    printf("Zadaj integer: ");
+    scanf("%i", &cislo);
+    printf("Tvoj integer je %i\n", cislo);
+
+    long velkeCislo;
+    printf("Zadaj velky integer: ");
+    scanf("%li", &velkeCislo);
+    printf("Tvoj integer je %li\n", velkeCislo);
+
+    long long obrovskeCislo;
+    printf("Zadaj obrovsky integer: ");
+    scanf("%lli", &obrovskeCislo);
+    printf("Tvoj integer je %lli\n", obrovskeCislo);
+
+    float desatinne1;
+    printf("Zadaj float: ");
+    scanf("%f", &desatinne1);
+    printf("Tvoj float je %f\n", desatinne1);
+
+    double desatinne2;
+    printf("Zadaj double: ");
+    scanf("%lf", &desatinne2);
+    printf("Tvoj double je %lf\n", desatinne2);
+}
