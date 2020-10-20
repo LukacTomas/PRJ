39473cee5322216561bac6d4994200d3467d588d
Tomas Lukac <39467273+LukacTomas@users.noreply.github.com>
2020-10-20 20:48:15 +0200

    Create logic-relational.c

---
+++ uvod_do_C/logic-relational.c
@@ -0,0 +1,27 @@
+/**
+ * Subor:   logic-relational.c
+ *
+ * Autor:  Tomas Lukac (lukac@fajnorka.sk)
+ * Datum:   16. 10. 2020
+ * Partner: Pracoval som sám
+ * Predmet: PRJ 2020
+ * Trieda:  ucitel
+ *
+ * Opis suboru:
+ *
+ *  Priklad na logicke a relacne operatory
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+   int vek = 19;
+
+   int test = vek >= 13 && vek <= 19;
+
+   printf("V teste sa nachadza %i\n", test);
+
+}
